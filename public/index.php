ad5165c9d4fa6c5ef83d8c02cf4103588b6d01c0
NathanGRSDesktop <nathangeers00@gmail.com>
2026-05-31 10:20:54 +0200

    Add database connection and initial project files

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Nathan Geers | Portfolio</title>
+</head>
+<body>
+    <h1>My Portfolio</h1>
+
+    <div id="container"></div>
+</body>
+</html>
+
